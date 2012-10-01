39fa815f82a6845cce4caa030050d953c1d75447
kkosman <kkosman@e386fb0d-b84c-48f7-add2-980c30d93de2>
2012-10-01 11:19:54 +0000

    All assets compile

--- lib/l/rails/engine.rb
+++ lib/l/rails/engine.rb
@@ -2,7 +2,7 @@ module L
   module Rails
     class Engine < ::Rails::Engine
       initializer "precompile", :group => :all do |app|
-        app.config.assets.precompile += ["admins.js", "admins.css", 'admin/admin_login.css']
+        app.config.assets.precompile += ["jquery.js", "jquery_ujs.js", "admin.js", "admins.js", "admins.css", 'admin/admin_login.css']
       end
     end
   end
