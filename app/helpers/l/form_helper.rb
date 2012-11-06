a1cce98bbda644e72fe8870af68717fc6ab7264f
bbulat <bbulat@e386fb0d-b84c-48f7-add2-980c30d93de2>
2012-11-06 13:34:21 +0000

    Poprawki sort_tag helpera. Wersja 3.14

--- app/helpers/l/form_helper.rb
+++ app/helpers/l/form_helper.rb
@@ -7,11 +7,17 @@ module L
     end
 
     def sort_tag(name, type, options = {})
-      options.merge!({
-        class: (type == :asc ? 'up' : 'down'),
+      options = options.reverse_merge({
+        class: [],
         name: 'sort',
         title:I18n.t("sort.#{type.to_s}")
       })
+
+      if options[:class].is_a? String
+        options[:class] = [options[:class], (type == :asc ? 'up' : 'down')].uniq
+      elsif options[:class].is_a? Enumerable
+        options[:class].push(type == :asc ? 'up' : 'down').uniq!
+      end
 
       submit_tag "#{name.to_s}_#{type.to_s}", options
     end
--- l.gemspec
+++ l.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = "l"
-  s.version = "0.3.13"
+  s.version = "0.3.14"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Bartek Bulat", "Krzystof Kosman"]
-  s.date = "2012-11-05"
+  s.date = "2012-11-06"
   s.description = "Bunch of generators for lazy programmers"
   s.email = "admin@1000i.pl"
   s.extra_rdoc_files = [
