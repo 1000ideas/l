1592ff058a6c8fb65fb913afc73750e10f809cdd
Bartek Bulat <bartek@1000i.pl>
2013-12-10 11:30:22 +0100

    Quick fix lazy_user validations

--- lib/l/concerns/lazy_user.rb
+++ lib/l/concerns/lazy_user.rb
@@ -2,9 +2,8 @@ module L
   module Concerns
     # Moduł rozszerzający klase uzytkownika. Dodaje domyślnie role (admin i user).
     # Automatycznie ładuje moduły devise'a [:database_authenticatable, :timeoutable,
-    # :rememberable, :trackable, :registerable]. Dodaje accesible dla atrybutów:
-    # :email, :password, :password_confirmation, :remember_me. Definuje walidacje
-    # dla :email i :password.
+    # :rememberable, :trackable, :registerable, :validatable]. Dodaje accesible dla atrybutów:
+    # :email, :password, :password_confirmation, :remember_me.
     module LazyUser
       extend ActiveSupport::Concern
 
@@ -12,13 +11,12 @@ module L
         has_role :admin, :user, default: :user
         
         devise :database_authenticatable, :timeoutable,
-               :rememberable, :trackable, :registerable
+               :rememberable, :trackable, :registerable,
+               :validatable
 
         attr_accessible :email, :password, :password_confirmation, :remember_me
 
-        validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
-        validates :password, presence: true, confirmation: true, length: {minimum: 5}
-      end
+        end
 
     end
   end
